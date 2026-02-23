0d8d4ebcb1f95141b3c72fc9a0f0607d49289d2e
BorobhaiRion <BorobhaiRion@users.noreply.github.com>
2026-02-24 03:24:08 +0600

    Added the inpuut loic for name and cpa

--- lab3(html css and js)/JS/Script.js
+++ lab3(html css and js)/JS/Script.js
@@ -1,1 +1,6 @@
-window.alert("WELLCOME TO MY REG PAGE)
+function ShowInfo(){
+    let  sname = document.getElementById("sName").value;
+    let sGpa = document.getElementById("sGpa").value;
+
+    window.alert(sname+"'s Cgpa is "+sGpa);
+}
