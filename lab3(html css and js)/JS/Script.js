d5a2ccd72145c9809844cf1789271217d7e58f0d
BorobhaiRion <BorobhaiRion@users.noreply.github.com>
2026-02-25 01:42:04 +0600

    Tried out j by takin some input

--- lab3(html css and js)/JS/Script.js
+++ lab3(html css and js)/JS/Script.js
@@ -1,6 +1,25 @@
-function ShowInfo(){
-    let  sname = document.getElementById("sName").value;
-    let sGpa = document.getElementById("sGpa").value;
 
-    window.alert(sname+"'s Cgpa is "+sGpa);
+
+function ShowInfo(){
+    let  sname = document.getElementById("sName").value.trim();
+    let sGpa = document.getElementById("sGpa").value.trim();
+
+    let errors = [];
+
+    if(sname == ""){
+        errors.push("Student Name Is Required!");
+    }
+
+    if(sGpa == ""){
+        errors.push("Student CGPA Is Required!");
+    }
+
+    if(errors.length > 0){
+        alert(errors.join("\n"));
+        return;
+    }
+
+    alert(sname +"'s Gpa Is :"+sGpa);
+    
+   
 }
