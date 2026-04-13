dbba98d7363adb2409318fa32889f337bd770975
BorobhaiRion <BorobhaiRion@users.noreply.github.com>
2026-04-14 00:23:00 +0600

    request and validation, including email and website using preg-Match()

--- Final/lab1 php/controller/RegistrationValidation.php
+++ Final/lab1 php/controller/RegistrationValidation.php
@@ -13,11 +13,11 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     $website = $_POST["website"];
     $comment = $_POST["comment"];
 
-    // use isset for radio button
     if(isset($_POST["gender"]))
     {
         $gender = $_POST["gender"];
     }
+
 
     $name = $_REQUEST["name"];
     $email = $_REQUEST["email"];
@@ -29,6 +29,53 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
         $gender = $_REQUEST["gender"];
     }
 
+
+
+    if(!empty($name) && strlen($name)>=5)
+    {
+        echo "User Name: ".$name."<br>";
+    }
+    else
+    {
+        echo "User-Name must be greater than 5 char<br>";
+    }
+
+    if(!empty($email) && preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email))
+    {
+        echo "Email: ".$email."<br>";
+    }
+    else
+    {
+        echo "Invalid Email<br>";
+    }
+
+if(!empty($website) && preg_match("/^(https?:\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $website))
+{
+    echo "Website: ".$website."<br>";
+}
+else
+{
+    echo "Invalid Website URL<br>";
+}
+
+    if(!empty($comment))
+    {
+        echo "Comment: ".$comment."<br>";
+    }
+    else
+    {
+        echo "comment cannot be EEmpty<br>";
+    }
+
+    
+    if(isset($gender) && !empty($gender))
+    {
+        echo "Gender: ".$gender."<br>";
+    }
+    else
+    {
+        echo "please select a Gender<br>";
+    }
 }
 
 ?>
