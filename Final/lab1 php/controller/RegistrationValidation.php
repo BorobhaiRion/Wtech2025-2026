598e599291f239d3377a9be672d64f7ea3e5afbf
BorobhaiRion <BorobhaiRion@users.noreply.github.com>
2026-04-20 13:27:53 +0600

    New changes using session starts()

--- Final/lab1 php/controller/RegistrationValidation.php
+++ Final/lab1 php/controller/RegistrationValidation.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+session_start();
 
 $name = "";
 $email = "";
@@ -13,69 +14,62 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
     $website = $_POST["website"];
     $comment = $_POST["comment"];
 
-    if(isset($_POST["gender"]))
-    {
+    if(isset($_POST["gender"])) {
         $gender = $_POST["gender"];
     }
 
-
-    // $name = $_REQUEST["name"];
-    // $email = $_REQUEST["email"];
-    // $website = $_REQUEST["website"];
-    // $comment = $_REQUEST["comment"];
-
-    // if(isset($_REQUEST["gender"]))
-    // {
-    //     $gender = $_REQUEST["gender"];
-    // }
-
-
-
-    if(!empty($name) && strlen($name)>=5)
-    {
-        echo "User Name: ".$name."<br>";
-    }
-    else
-    {
-        echo "User-Name must be greater than 5 char<br>";
-    }
-
-    if(!empty($email) && preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email))
-    {
-        echo "Email: ".$email."<br>";
-    }
-    else
-    {
-        echo "Invalid Email<br>";
-    }
-
-if(!empty($website) && preg_match("/^(https?:\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $website))
-{
-    echo "Website: ".$website."<br>";
-}
-else
-{
-    echo "Invalid Website URL<br>";
-}
-
-    if(!empty($comment))
-    {
-        echo "Comment: ".$comment."<br>";
-    }
-    else
-    {
-        echo "comment cannot be EEmpty<br>";
-    }
-
     
-    if(isset($gender) && !empty($gender))
-    {
-        echo "Gender: ".$gender."<br>";
+    $isValid = true;
+
+    if(!empty($name) && strlen($name) >= 5) {
+        echo "User Name: ".$name."<br>";
+    } else {
+        echo "User-Name must be greater than 5 char<br>";
+        $isValid = false;
     }
-    else
-    {
-        echo "please select a Gender<br>";
+
+    if(!empty($email) && preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $email)) {
+        echo "Email: ".$email."<br>";
+    } else {
+        echo "Invalid Email<br>";
+        $isValid = false;
+    }
+
+    if(!empty($website) && preg_match("/^(https?:\/\/)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/", $website)) {
+        echo "Website: ".$website."<br>";
+    } else {
+        echo "Invalid Website URL<br>";
+        $isValid = false;
+    }
+
+    if(!empty($comment)) {
+        echo "Comment: ".$comment."<br>";
+    } else {
+        echo "Comment cannot be empty<br>";
+        $isValid = false;
+    }
+
+    if(isset($gender) && !empty($gender)) {
+        echo "Gender: ".$gender."<br>";
+    } else {
+        echo "Please select a Gender<br>";
+        $isValid = false;
+    }
+
+    if($isValid) {
+        $_SESSION['name'] = $name;
+        setcookie('name', $name, time()+200, '/');
+
+        echo "<br>Welcome! {$name}<br>";
+    } else {
+        echo "<br>Reload the page!<br>";
     }
 }
 
+
+if(isset($_SESSION['name']) || isset($_COOKIE['name'])) {
+    echo "Cookie and session set";
+} else {
+    echo "Re-submit the form";
+}
 ?>
